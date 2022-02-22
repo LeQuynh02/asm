58a2772375988db3493454b920cfb9f87a3aa755
LeQuynh02 <93896744+LeQuynh02@users.noreply.github.com>
2022-02-22 17:37:31 +0700

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,7 +13,7 @@ const footer = {
                             <form class="footernews_form">
                                 <input type="text" placeholder="Your email address" class="py-[10px] rounded-lg bg-none">
                                 <a href="#">
-                                    <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sing in</button>
+                                    <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Đăng ký</button>
                                 </a>
                             </form>
                         </div>
