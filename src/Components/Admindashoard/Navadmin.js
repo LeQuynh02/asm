fc6086a3affb33131a9a71f67f1994da03d20774
LeQuynh02 <93896744+LeQuynh02@users.noreply.github.com>
2022-02-23 16:04:16 +0700

    update

--- src/Components/Admindashoard/Navadmin.js
+++ src/Components/Admindashoard/Navadmin.js
@@ -19,6 +19,8 @@ const Navadmin = {
                                     <a href="/admin/dashboard" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Settings</a>
 
                                     <a href="/admin/dashboard" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Order</a>
+
+                                    <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Trang chủ</a>
                                 </div>
                             </div>
                         </div>
