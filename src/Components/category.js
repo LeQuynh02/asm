99ac93f21816ae9ab3c9621f9877bfd6d8ae0b7f
LeQuynh02 <93896744+LeQuynh02@users.noreply.github.com>
2022-02-25 14:21:19 +0700

    update

--- src/Components/category.js
+++ src/Components/category.js
@@ -1,30 +1,48 @@
 const navcatory = {
-    render() {
-        return /* html */ `
-     
-                 <ul class="border-solid border-2 border[#f3f3f3]">
-                <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">PC gameming</a></li>
-              <hr>
-                    <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">LapTop HP</a></li>
-              <hr>
-                    <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">LapTop Dell</a></li>
-              <hr>
-                    <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">LapTop Gaming</a></li>
-              <hr>
-                    <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">LapTop Asus</a></li>
-              <hr>
-                    <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">LapTop LPX</a></li>
-              <hr>
-                    <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">LapTop HXL</a></li>
-              <hr>
-                    <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] "><a class="" href="">LapTop ỊK</a></li>
-              <hr>
-              </ul>
+render() {
+return /* html */ `
 
-        
-        
-        `;
-    },
+<ul class="border-solid border-2 border[#f3f3f3]">
+      <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] ">
+            <a href="" class="mt-3 text-center hover:font-bold">
+                  <img class="phone-color" src="https://vn-live-03.slatic.net/p/baa684a6f98b465db56c190e801ee2b7.jpg">
+                  <p class="ml-3">Mẫu 2</p>
+            </a>
+      </li>
+      <hr>
+      <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] ">
+            <a href="" class="mt-3 text-center hover:font-bold">
+                  <img class="phone-color" src="https://vn-live-03.slatic.net/p/baa684a6f98b465db56c190e801ee2b7.jpg">
+                  <p class="ml-3">Mẫu 2</p>
+            </a>
+      </li>
+      <hr>
+      <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] ">
+            <a href="" class="mt-3 text-center hover:font-bold">
+                  <img class="phone-color" src="https://vn-live-03.slatic.net/p/baa684a6f98b465db56c190e801ee2b7.jpg">
+                  <p class="ml-3">Mẫu 2</p>
+            </a>
+      </li>
+      <hr>
+      <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] ">
+            <a href="" class="mt-3 text-center hover:font-bold">
+                  <img class="phone-color" src="https://vn-live-03.slatic.net/p/baa684a6f98b465db56c190e801ee2b7.jpg">
+                  <p class="ml-3">Mẫu 2</p>
+            </a>
+      </li>
+      <hr>
+      <li class="px-[30px] py-[10px]  hover:bg-[#f3f3f3] ">
+            <a href="" class="mt-3 text-center hover:font-bold">
+                  <img class="phone-color" src="https://vn-live-03.slatic.net/p/baa684a6f98b465db56c190e801ee2b7.jpg">
+                  <p class="ml-3">Mẫu 2</p>
+            </a>
+      </li>
+</ul>
+
+
+
+`;
+},
 
 };
 export default navcatory;
