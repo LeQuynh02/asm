0cdfa5a64d8f1e13fff8d89c9780079e159fe7d1
LeQuynh02 <93896744+LeQuynh02@users.noreply.github.com>
2022-02-25 14:22:30 +0700

    update

--- src/Pages/products/cart.js
+++ src/Pages/products/cart.js
@@ -25,13 +25,13 @@ const CartPage = {
                     <thead>
                         <tr class="h-12 uppercase">
                             <th class="hidden md:table-cell"></th>
-                            <th class="text-left">Product</th>
+                            <th class="text-left">Sản phẩm</th>
                             <th class="lg:text-right text-left pl-5 lg:pl-0">
                                 <span class="lg:hidden" title="Quantity">Qtd</span>
-                                <span class="hidden lg:inline">Quantity</span>
+                                <span class="hidden lg:inline">Số lượng</span>
                             </th>
-                            <th class="hidden text-right md:table-cell">Unit price</th>
-                            <th class="text-right">Total price</th>
+                            <th class="hidden text-right md:table-cell">Giá</th>
+                            <th class="text-right"></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -76,11 +76,7 @@ const CartPage = {
                 <div class="my-4 mt-6 -mx-2 lg:flex">
 
                     <div class="lg:px-2 lg:w-1/2">
-                        <div class="p-4 bg-gray-100 rounded-full">
-                            <h1 class="ml-2 font-bold uppercase">Order Details</h1>
-                        </div>
                         <div class="p-4">
-                            <p class="mb-6 italic">Shipping and additionnal costs are calculated based on values you have entered</p>
                             <div class="flex justify-between border-b">
                                 <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
                                     Tổng tiền
@@ -89,11 +85,13 @@ const CartPage = {
                                   ${tong}
                                 </div>
                             </div>
+                            <br>
+                            <p class="mb-6 italic">Miễn phí ship trên toàn quốc</p>
 
                             <a href="#">
                                 <button class="flex justify-center w-full px-10 py-3 mt-6 font-medium text-white uppercase bg-gray-800 rounded-full shadow item-center hover:bg-gray-700 focus:shadow-outline focus:outline-none">
                   <svg aria-hidden="true" data-prefix="far" data-icon="credit-card" class="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z"/></svg>
-                  <span class="ml-2 mt-5px">Procceed to checkout</span>
+                  <span class="ml-2 mt-5px">Thanh toán</span>
                 </button>
                             </a>
                         </div>
